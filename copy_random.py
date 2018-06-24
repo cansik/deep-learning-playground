a0206daab17fd2c04dffd410154ac8f7c51b5c54
Florian Bruggisser <florian@nexpose.ch>
2018-06-24 11:06:38 +0200

    added groundtruth creation

--- copy_random.py
+++ copy_random.py
@@ -10,28 +10,35 @@ from shutil import copyfile
 working_dir = os.path.dirname(os.path.realpath(__file__))
 default_extension = ""
 
+groundtruth_name = "groundtruth.csv"
+
 # parse arguments
 parser = argparse.ArgumentParser(description='Copies random files to new destination.')
 parser.add_argument('src', help='Source folder')
 parser.add_argument('dest', help='Destination folder')
 parser.add_argument('count', help='Amount of files to be copied')
+parser.add_argument('--groundtruth', default=False, help='Add groundtruth file')
 parser.add_argument('--hidden', default=False, help='Allow hidden files')
 parser.add_argument('--extension', default=default_extension, help='File extension to copy')
 
 args = parser.parse_args()
 
+# read arguments
 src_path = os.path.abspath(args.src)
 dest_path = os.path.abspath(args.dest)
 count = int(args.count)
+extension = args.extension
+is_hidden = bool(args.hidden)
+is_groundtruth = bool(args.groundtruth)
 
 # read files
 print("read files...")
-files = os.listdir(src_path)
+files = sorted(os.listdir(src_path))
 
-if args.extension != "":
-    files = filter(lambda x: x.endswith(args.extension), files)
+if extension != "":
+    files = filter(lambda x: x.endswith(extension), files)
 
-if not args.hidden:
+if not is_hidden:
     files = filter(lambda x: not x.startswith("."), files)
 
 print("files %s found" % len(files))
@@ -39,7 +46,10 @@ print("files %s found" % len(files))
 #n generate indexes
 print("generate indexes...")
 indexes = random.sample(range(0, len(files)), count)
+indexes = sorted(indexes)
 print("indexes: %s" % indexes)
+
+groundtruth_content = ""
 
 print('copy files...')
 for i in indexes: 
@@ -49,5 +59,13 @@ for i in indexes:
     src_file_path = os.path.join(src_path, name)
     dest_file_path = os.path.join(dest_path, name)
     copyfile(src_file_path, dest_file_path)
+
+    groundtruth_content += "%s,\n" % name
+
+
+if is_groundtruth:
+    groundtruth_file = os.path.join(dest_path, groundtruth_name)
+    with open(groundtruth_file, "w") as text_file:
+        text_file.write(groundtruth_content)
 
 exit(0)
